557ab4a44d8ffb480edf1d7ede6887334af7a8c8
iPhoneGang <iphonegang@iphonegangs-mac.local>
2016-03-05 08:29:36 -0800

    Initial Commit

---
+++ SimpleCamScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleCamScanner
+//
+//  Created by iPhoneGang on 3/5/16.
+//  Copyright © 2016 iPhone Max Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
